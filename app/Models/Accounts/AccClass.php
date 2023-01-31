0274ae4ce3aebf3fd590534275db728e5eb399fa
Ismail0184 <ismailhossain600@gmail.com>
2023-01-31 15:10:10 +0600

    update

---
+++ app/Models/Accounts/AccClass.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models\Accounts;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AccClass extends Model
+{
+    use HasFactory;
+
+    public static $classes;
+
+    public static function storeClass($request)
+    {
+        self::$classes = new AccClass();
+        self::$classes->class_id = $request->class_id;
+        self::$classes->class_name = $request->class_name;
+        self::$classes->status = 1;
+        self::$classes->save();
+
+    }
+
+    public static function updateClass($request, $id)
+    {
+        self::$classes = AccClass::find($id);
+        self::$classes->class_id = $request->class_id;
+        self::$classes->class_name = $request->class_name;
+        self::$classes->status = $request->status;
+        self::$classes->save();
+    }
+
+    public static function destroyClass($id)
+    {
+        self::$classes = AccClass::find($id);
+        self::$classes->delete();
+    }
+}
---
+++ database/migrations/2023_01_31_042111_create_acc_classes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('acc_classes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('class_id')->unique();
+            $table->string('class_name', 33)->unique();
+            $table->integer('status')->default('1');
+            $table->integer('scomid')->default('1');
+            $table->integer('pcomid')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('acc_classes');
+    }
+};
