de03d26352ab5cd6ad074df5b2f707af7e844b5d
Ismail0184 <ismailhossain600@gmail.com>
2023-02-08 11:35:33 +0600

    update

---
+++ app/Models/Accounts/AccLedger.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Accounts;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AccLedger extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_02_08_052458_create_acc_ledgers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('acc_ledgers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('ledger_id')->unique();
+            $table->string('ledger_name')->unique();
+            $table->bigInteger('group_id');
+            $table->integer('sub_class_id');
+            $table->integer('class_id');
+            $table->integer('status')->default('1');
+            $table->integer('sconid');
+            $table->integer('pcomid');
+            $table->integer('entry_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('acc_ledgers');
+    }
+};
