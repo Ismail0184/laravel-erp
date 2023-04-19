95365ff54c16e88ac3d709334cef667ff2b34662
Ismail0184 <ismailhossain600@gmail.com>
2023-04-19 09:58:38 +0600

    created developer

---
+++ app/Models/Developer/DevCompany.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Developer;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DevCompany extends Model
+{
+    use HasFactory;
+}
