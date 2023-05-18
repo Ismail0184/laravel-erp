256debca9fb1ca28042a9e4e11112e70f7b63bc2
Ismail0184 <ismailhossain600@gmail.com>
2023-05-18 15:04:26 +0600

    sub sub ledger

--- resources/views/modules/accounts/coa/subsubledger/index.blade.php
+++ resources/views/modules/accounts/coa/subsubledger/index.blade.php
@@ -23,27 +23,27 @@
                             <tr>
                                 <th style="width: 5%; text-align: center">#</th>
                                 <th style="width: 5%; text-align: center">Uid</th>
+                                <th>Sub-Sub-Ledger</th>
                                 <th>Sub-Ledger</th>
-                                <th>Ledger</th>
                                 <th>Status</th>
                                 <th class="text-center" style="width: 15%">Option</th>
                             </tr>
                             </thead>
                             <tbody>
-                            @foreach($subsubledgers as $subsubledge)
+                            @foreach($subsubledgers as $subsubledger)
                                 <tr>
                                     <td style="text-align: center">{{$loop->iteration}}</td>
-                                    <td style="text-align: center">{{$subledger->id}}</td>
-                                    <td>{{$subledger->sub_ledger_id}} : {{$subledger->sub_ledger_name}}</td>
-                                    <td>{{$subledger->ledger_id}} : {{$subledger->ledger_name}}</td>
-                                    <td>@if($subledger->status == '1') <span class="badge badge-success">Active</span> @elseif($ledger->status == '0') <span class="badge badge-danger">Inactive</span> @endif</td>
+                                    <td style="text-align: center">{{$subsubledger->id}}</td>
+                                    <td>{{$subsubledger->sub_ledger_id}} : {{$subsubledger->sub_sub_ledger_name}}</td>
+                                    <td>{{$subsubledger->ledger_id}} : {{$subsubledger->ledger_name}}</td>
+                                    <td>@if($subsubledger->status == '1') <span class="badge badge-success">Active</span> @elseif($ledger->status == '0') <span class="badge badge-danger">Inactive</span> @endif</td>
                                     <td class="text-center">
-                                        <form action="{{route('acc.sub-ledger.destroy', ['sub_ledger_id' => $subledger->sub_ledger_id])}}" method="post">
+                                        <form action="{{route('acc.sub-ledger.destroy', ['sub_ledger_id' => $subsubledger->sub_ledger_id])}}" method="post">
                                             @csrf
-                                            <a href="{{route('acc.sub-ledger.show',['sub_ledger_id' => $subledger->sub_ledger_id])}}" title="View" class="btn btn-primary btn-sm">
+                                            <a href="{{route('acc.sub-ledger.show',['sub_ledger_id' => $subsubledger->sub_ledger_id])}}" title="View" class="btn btn-primary btn-sm">
                                                 <i class="fa fa-book"></i>
                                             </a>
-                                            <a href="{{route('acc.sub-ledger.edit',['sub_ledger_id' => $subledger->sub_ledger_id])}}" title="Update" class="btn btn-success btn-sm">
+                                            <a href="{{route('acc.sub-ledger.edit',['sub_ledger_id' => $subsubledger->sub_ledger_id])}}" title="Update" class="btn btn-success btn-sm">
                                                 <i class="fa fa-edit"></i>
                                             </a>
                                             <button type="submit" class="btn btn-danger btn-sm" title="Delete" onclick="return confirm('Are you confirm to delete?');">
