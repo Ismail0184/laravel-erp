d4134ff28e9fb6531aa02fe8c439f8fe73c08b33
Ismail0184 <ismailhossain600@gmail.com>
2023-11-22 09:54:41 +0600

    updated

--- app/Models/HRM/employee/HrmEmployeeFamilyInfo.php
+++ app/Models/HRM/employee/HrmEmployeeFamilyInfo.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class HrmEmployeeFamilyInfo extends Model
 {
     use HasFactory;
+
+    public static $employee;
+
+    public static function storeFamilyInfo($request)
+    {
+        self::$employee = new HrmEmployeeFamilyInfo();
+        self::$employee->employee_id = $request->employee_id;
+        self::$employee->name = $request->name;
+        self::$employee->relationship = $request->relationship;
+        self::$employee->nid = $request->nid;
+        self::$employee->mobile = $request->mobile;
+        self::$employee->email = $request->email;
+        self::$employee->profession = $request->profession;
+        self::$employee->entry_by = $request->entry_by;
+        self::$employee->sconid = 1;
+        self::$employee->pcomid = 1;
+        self::$employee->save();
+    }
+
+    public static function updateFamilyInfo($request, $id)
+    {
+        self::$employee = HrmEmployeeFamilyInfo::where('employee_id', $id)->firstOrfail();;
+        self::$employee->employee_id = $request->employee_id;
+        self::$employee->name = $request->name;
+        self::$employee->relationship = $request->relationship;
+        self::$employee->nid = $request->nid;
+        self::$employee->mobile = $request->mobile;
+        self::$employee->email = $request->email;
+        self::$employee->profession = $request->profession;
+        self::$employee->updated_by = $request->entry_by;
+        self::$employee->sconid = 1;
+        self::$employee->pcomid = 1;
+        self::$employee->save();
+    }
 }
