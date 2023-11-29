97d50180193b5b1b92baa4cb6f629bf1410b2395
Ismail0184 <ismailhossain600@gmail.com>
2023-11-29 15:01:33 +0600

    updated

--- app/Models/HRM/payroll/HrmPayrollSalaryScale.php
+++ app/Models/HRM/payroll/HrmPayrollSalaryScale.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class HrmPayrollSalaryScale extends Model
 {
     use HasFactory;
+
+    public static $salaryScale;
+
+    public static function storeSalaryScale($request)
+    {
+        self::$salaryScale = new HrmPayrollSalaryScale();
+        self::$salaryScale->employee_id = $request->employee_id;
+        self::$salaryScale->education_degree = $request->education_degree;
+        self::$salaryScale->education_subject = $request->education_subject;
+        self::$salaryScale->grade = $request->grade;
+        self::$salaryScale->passing_year = $request->passing_year;
+        self::$salaryScale->cgpa = $request->cgpa;
+        self::$salaryScale->scale = $request->scale;
+        self::$salaryScale->institute = $request->institute;
+        self::$salaryScale->last_education = $request->last_education;
+        self::$salaryScale->institute_type = $request->institute_type;
+        self::$salaryScale->entry_by = $request->entry_by;
+        self::$salaryScale->sconid = 1;
+        self::$salaryScale->pcomid = 1;
+        self::$salaryScale->save();
+    }
+
+    public static function destroySalaryScale($id)
+    {
+        self::$salaryScale = HrmPayrollSalaryScale::findOrfail($id);
+        self::$salaryScale->delete();
+
+    }
 }
--- resources/views/modules/hrm/payroll/salaryScale/create.blade.php
+++ resources/views/modules/hrm/payroll/salaryScale/create.blade.php
@@ -9,66 +9,55 @@
     <div class="col-lg-12">
         <div class="card">
             <div class="card-body">
-                <h4 class="card-title mb-4">@if(request('id')) Update @else Create @endif {{$title}} <small class="text-danger float-right"> field marked with * are mandatory</small></h4>
+                <h4 class="card-title mb-4">@if(request('id')) Update @else Create @endif {{$title}}</h4>
                 <form method="POST" action="@if(request('id')>0) {{route('hrm.employee.update', ['id'=>$employee->id])}} @else {{route('hrm.employee.store')}} @endif" enctype="multipart/form-data">
                     @csrf
                     <input type="hidden" name="entry_by" value="{{ Auth::user()->id }}">
 
                     <div class="form-group row mb-2">
-                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Basic Amount <span class="required text-danger">*</span></label>
-                        <div class="col-sm-5">
+                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Basic Amount</label>
+                        <div class="col-sm-9">
                             <input type="text" name="basic_amount" @if(request('code')>0) value="{{$employee->basic_amount}}" @endif class="form-control" required>
                         </div>
                     </div>
                     <div class="form-group row mb-2">
                         <label for="horizontal-email-input" class="col-sm-3 col-form-label">House Rent</label>
-                        <div class="col-sm-5">
+                        <div class="col-sm-9">
                             <input type="text" name="house_rent" @if(request('code')>0) value="{{$employee->house_rent}}" @endif class="form-control">
                         </div>
                     </div>
                     <div class="form-group row mb-2">
                         <label for="horizontal-email-input" class="col-sm-3 col-form-label">Conveyance Bill</label>
-                        <div class="col-sm-5">
+                        <div class="col-sm-9">
                             <input type="text" name="conveyance_bill" @if(request('code')>0) value="{{$employee->conveyance_bill}}" @endif class="form-control">
                         </div>
                     </div>
                     <div class="form-group row mb-2">
-                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Spouse's Name</label>
+                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Phone Bill</label>
                         <div class="col-sm-9">
-                            <input type="text" name="spouse_name" @if(request('code')>0) value="{{$employee->spouse_name}}" @endif class="form-control">
+                            <input type="text" name="phone_bill" @if(request('code')>0) value="{{$employee->phone_bill}}" @endif class="form-control">
                         </div>
                     </div>
                     <div class="form-group row mb-2">
-                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Date of Birth <span class="required text-danger">*</span></label>
+                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Medical Allowance</label>
                         <div class="col-sm-9">
-                            <input type="date" name="date_of_birth" @if(request('code')>0) value="{{$employee->date_of_birth}}" @endif class="form-control">
+                            <input type="text" name="medical_allowance" @if(request('code')>0) value="{{$employee->medical_allowance}}" @endif class="form-control">
                         </div>
                     </div>
 
                     <div class="form-group row mb-2">
-                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">National ID <span class="required text-danger">*</span></label>
+                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Income Tax</label>
                         <div class="col-sm-9">
-                            <input type="text" name="national_id" @if(request('code')>0) value="{{$employee->national_id}}" @endif required class="form-control">
+                            <input type="text" name="income_tax" @if(request('code')>0) value="{{$employee->income_tax}}" @endif required class="form-control">
                         </div>
                     </div>
-                    <div class="form-group row mb-2">
-                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Birth Certificate ID <span class="required text-danger">*</span></label>
+                    <div class="form-group row mb-4">
+                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Provident Fund</label>
                         <div class="col-sm-9">
-                            <input type="text" name="birth_certificate_id" @if(request('code')>0) value="{{$employee->birth_certificate_id}}" @endif required class="form-control">
+                            <input type="text" name="pf_amount" @if(request('code')>0) value="{{$employee->pf_amount}}" @endif required class="form-control">
                         </div>
                     </div>
-                    <div class="form-group row mb-2">
-                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Passport No</label>
-                        <div class="col-sm-9">
-                            <input type="text" name="passport_id" @if(request('code')>0) value="{{$employee->passport_id}}" @endif class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row mb-2">
-                        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Driving License</label>
-                        <div class="col-sm-9">
-                            <input type="text" name="driving_license" @if(request('code')>0) value="{{$employee->driving_license}}" @endif class="form-control">
-                        </div>
-                    </div>
+
 
                     @if(request('id')>0)
                         <div class="form-group row mb-4">
